dad85dc5fc23e23417a07219f06694476d8f57b6
shivakar005 <shivakar005@gmail.com>
2024-10-30 00:08:16 +0530

    home page design done

--- assets/js/stepdata.js
+++ assets/js/stepdata.js
@@ -1,7 +1,7 @@
 const stepData = [
     {
         title: '1986',
-        description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt.',
+        description: 'Incorporated as Private Limited Company',
     },
     {
         title: '',
@@ -9,71 +9,71 @@ const stepData = [
     },
     {
         title: '1987',
-        description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt.',
+        description: 'First plant commissioned',
     },
     {
         title: '1994',
-        description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt.',
+        description: 'Converted into Public Limited Company',
     },
     {
         title: '1995',
-        description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt.',
+        description: 'Got listed on stock exchange',
     },
     {
         title: '1997',
-        description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt.',
+        description: 'Capacity expansion to 50,000 MTPA',
     },
     {
         title: '2003',
-        description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt.',
+        description: 'Turnover crossed USD 31 Mn',
     },
     {
         title: '2006',
-        description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt.',
+        description: 'Commissioned first forging plant',
     },
     {
         title: '2007',
-        description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt.',
-    },
-    {
-        title: '2018',
-        description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt.',
-    },
-    {
-        title: '2016',
-        description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt.',
-    },
-    {
-        title: '2015',
-        description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt.',
-    },
-    {
-        title: '2014',
-        description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt.',
+        description: 'Commissioned first plant for ERW and CDW Precision Tubes',
     },
     {
         title: '2013',
-        description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt.',
+        description: 'Turnover crossed USD 185 Mn',
+    },
+    {
+        title: '2014',
+        description: 'Commissioned Second plant for ERW and CDW Precision Tubes',
+    },
+    {
+        title: '2015',
+        description: 'Expanded Engineering structured products to high growth sectors like solar and railways',
+    },
+    {
+        title: '2016',
+        description: 'Company name changed to Goodluck India Limited',
+    },
+    {
+        title: '2018',
+        description: 'Commissioned Kutch Plant for ERW Precision Tubes',
     },
     {
         title: '2020',
-        description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt.',
+        description: 'Obtained approval from RDSO* for Kutch Plant – Sikandrabad plant already approved',
     },
     {
         title: '2021',
-        description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt.',
+        description: 'Awarded Lol by L&T for High-Speed Railway Project -worth over USD 27 Mn for supply and fabrication of Special bridges & Enhanced Capacity for Precision Tubes',
     },
     {
         title: '2022',
-        description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt.',
+        description: 'Added new machinery - increase capacity of forging single piece to 14,000 Kgs and total capacity per annum to 30,000 MT',
     },
     {
         title: '2023',
-        description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt.',
+        description: 'Entered into Road Safety products by collaborating with a European Company',
     },
     {
         title: '2024',
-        description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt.',
+        description: 'Incorporated wholly owned subsidiary, Goodluck Defence and Aerospace Pvt Ltd to strengthen presence in defence sector exclusively',
     },
 
 ]
