2b38abec3ac986999b082f68459dcabdda3e9b47
shivakar005 <shivakar005@gmail.com>
2024-12-03 00:50:56 +0530

    css changes updated with scroll top

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -44,3 +44,22 @@ document.addEventListener('click', () => {
 });
 
 
+// Get the button
+const scrollTopBtn = document.getElementById("scrollTopBtn");
+
+// Show the button when the user scrolls down 20px from the top
+window.onscroll = function () {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollTopBtn.style.display = "flex";
+  } else {
+    scrollTopBtn.style.display = "none";
+  }
+};
+
+// Scroll to the top of the document when the button is clicked
+scrollTopBtn.addEventListener("click", function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
